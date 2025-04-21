8a6b815bac99f53d2a89f8874f7349e3d6175eb7
haw9k <144719920+haw9k@users.noreply.github.com>
2025-04-21 08:52:40 +0300

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/FormAddProduct.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WinFormsApp1
+{
+    partial class FormAddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.Location = new System.Drawing.Point(223, 219);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(100, 24);
+            this.buttonYes.TabIndex = 0;
+            this.buttonYes.Text = "Подтвердить";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.Location = new System.Drawing.Point(390, 217);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(72, 26);
+            this.buttonNo.TabIndex = 1;
+            this.buttonNo.Text = "Отмена";
+            this.buttonNo.UseVisualStyleBackColor = true;
+            this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(223, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(390, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(223, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Наименование";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(390, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ед. измерения";
+            // 
+            // FormAddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonNo);
+            this.Controls.Add(this.buttonYes);
+            this.Name = "FormAddProduct";
+            this.Text = "FormAddProduct";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonYes;
+        private Button buttonNo;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/FormAddProduct.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace WinFormsApp1
+{
+    public partial class FormAddProduct : Form
+    {
+        public NpgsqlConnection con;
+        int id;
+        public FormAddProduct(NpgsqlConnection con, int id)
+        {
+            InitializeComponent();
+            this.con = con;
+            this.id = id;
+        }
+        public FormAddProduct(NpgsqlConnection con, int id, string nameP, string ed)
+        {
+            InitializeComponent();
+            textBox1.Text = nameP;
+            textBox2.Text = ed;
+            this.con = con;
+            this.id = id;
+            
+        }
+        
+
+        private void buttonYes_Click(object sender, EventArgs e)
+        {
+            if (id == -1)
+            {
+                try
+                {
+                    NpgsqlCommand command = new NpgsqlCommand("INSERT INTO product(name, ed) VALUES(:name, :ed)", con);
+                    command.Parameters.AddWithValue("name", textBox1.Text);
+                    command.Parameters.AddWithValue("ed", textBox2.Text);
+                    command.ExecuteNonQuery();
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    NpgsqlCommand command = new NpgsqlCommand("UPDATE product SET name=:name, ed=:ed WHERE id=:id", con);
+                    command.Parameters.AddWithValue("id", id);
+                    command.Parameters.AddWithValue("name", textBox1.Text);
+                    command.Parameters.AddWithValue("ed", textBox2.Text);
+                    command.ExecuteNonQuery();
+                    Close();
+                }
+                catch{ }
+            }
+        }
+
+        private void buttonNo_Click(object sender, EventArgs e)
+        {
+            Close();    
+        }
+    }
+}
